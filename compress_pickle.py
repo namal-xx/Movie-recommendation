2451f3b724f3f51587da797da0be081898d3f218
namalrizwan <namalrizwan4@gamil.com>
2025-10-18 00:17:31 +0500

    modifed file

--- compress_pickle.py
+++ compress_pickle.py
@@ -1,5 +1,5 @@
 import joblib
 
 def create_compress_file():
-    data = joblib.load("data_dict")
+    data = joblib.load("data_dict.pkl")
     joblib.dump(data, "data_dict_compressed.pkl", compress=3)
